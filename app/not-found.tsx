11249c88016334ccd2d90f6ccee39f9db9d8c72d
Hugh Forte <hugh.forte@gmail.com>
2024-12-21 12:58:54 -0800

    TEMP: faux white list opacity classes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,6 +5,8 @@ export default function NotFound() {
         404 - Page Not Found
       </h1>
       <p className="mb-4">The page you are looking for does not exist.</p>
+      <div className="opacity-0 transition-opacity" />
+      <div className="opacity-100 transition-opacity" />
     </section>
   )
 }
