3c35a650b35249d86fb29ab01292ca46ba28484d
Hugh Forte <hugh.forte@gmail.com>
2024-12-21 09:21:30 -0800

    fix: add OpenSSL dependency to build script
    - Add OpenSSL build steps for git-crypt dependency
    - Set up LD_LIBRARY_PATH for shared libraries
    - Ensure proper directory navigation

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Create lib directory
+mkdir -p /tmp/lib
+
+# Download and extract OpenSSL 1.1
+curl -L -o /tmp/openssl.tar.gz https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1w.tar.gz
+tar xzf /tmp/openssl.tar.gz -C /tmp
+cd /tmp/openssl-OpenSSL_1_1_1w
+./config --prefix=/tmp/openssl
+make
+make install
+export LD_LIBRARY_PATH="/tmp/openssl/lib:$LD_LIBRARY_PATH"
+
 # Download pre-compiled git-crypt binary
 curl -L -o /tmp/git-crypt https://github.com/AGWA/git-crypt/releases/download/0.7.0/git-crypt-0.7.0-linux-x86_64
 chmod +x /tmp/git-crypt
@@ -11,4 +23,5 @@ git-crypt unlock /tmp/git-crypt-key
 rm /tmp/git-crypt-key
 
 # Run Next.js build
+cd /vercel/path0  # Return to project directory
 next build
